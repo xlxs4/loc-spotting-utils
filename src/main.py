4df3dda83bab84061166da6b42654ee3b05d0a07
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2022-11-13 23:10:12 +0200

    gui: Add label for selected G-Code path

--- src/main.py
+++ src/main.py
@@ -13,16 +13,18 @@ class GCodeUtils(QtWidgets.QWidget):
         self.init_ui()
 
     def init_ui(self) -> None:
+        self.selected_gcode_path = QtWidgets.QLabel("Selected G-Code: ")
+
+        self.browse_button = QtWidgets.QPushButton("Browse")
+        self.browse_button.clicked.connect(self.browse_gcode)
+
         self.gcode_viewer = QtWidgets.QPlainTextEdit()
         self.gcode_viewer.setReadOnly(True)
 
-        self.browse_button = QtWidgets.QPushButton("Browse")
-
         self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.gcode_viewer)
+        self.layout.addWidget(self.selected_gcode_path)
         self.layout.addWidget(self.browse_button)
-
-        self.browse_button.clicked.connect(self.browse_gcode)
+        self.layout.addWidget(self.gcode_viewer)
 
         self.gcode = None
 
@@ -34,6 +36,7 @@ class GCodeUtils(QtWidgets.QWidget):
 
         if dialog.exec():
             gcode_filename = dialog.selectedFiles()[0]
+            self.selected_gcode_path.setText(f"Selected G-Code: {gcode_filename}")
             self.gcode = read_gcode(gcode_filename)
 
 
