06c7060eb7ec1e5fae548accac4e787ec6db1f6c
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2022-11-14 23:40:40 +0200

    gui: Hook toolbar buttons with GCodeUtils coor functions

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import PySide6.QtGui as QtGui
 
 import PySide6.QtWidgets as QtWidgets
 
+from GCodeUtils import dec_coor, inc_coor, replace_coor
 from IOUtils import lines_to_text, read_gcode, write_gcode
 from paths import get_path
 
@@ -43,35 +44,56 @@ class GCodeUtils(QtWidgets.QMainWindow):
         toolbar.setIconSize(QtCore.QSize(ICON_WIDTH, ICON_HEIGHT))
         self.addToolBar(toolbar)
 
-        plus_button = QtGui.QAction(QtGui.QIcon(str(get_path("assets-plus"))), self.tr("Increase coordinate"), self)
-        plus_button.setCheckable(True)
+        plus_button = QtWidgets.QPushButton(QtGui.QIcon(str(get_path("assets-plus"))), "", self)
         plus_button.setStatusTip(self.tr("Increase X/Y/Z G-Code coordinates by value"))
-        plus_button.triggered.connect(self._aa)
+        plus_button.clicked.connect(self._handle_plus_button)
 
-        toolbar.addAction(plus_button)
+        toolbar.addWidget(plus_button)
         toolbar.addSeparator()
 
-        minus_button = QtGui.QAction(QtGui.QIcon(str(get_path("assets-minus"))), self.tr("Decrease coordinate"), self)
-        minus_button.setCheckable(True)
+        minus_button = QtWidgets.QPushButton(QtGui.QIcon(str(get_path("assets-minus"))), "", self)
         minus_button.setStatusTip(self.tr("Decrease X/Y/Z G-Code coordinates by value"))
-        minus_button.triggered.connect(self._aa)
+        minus_button.clicked.connect(self._handle_minus_button)
 
-        toolbar.addAction(minus_button)
+        toolbar.addWidget(minus_button)
         toolbar.addSeparator()
 
-        replace_button = QtGui.QAction(QtGui.QIcon(str(get_path("assets-replace"))), self.tr("Replace coordinate"), self)
-        replace_button.setCheckable(True)
+        replace_button = QtWidgets.QPushButton(QtGui.QIcon(str(get_path("assets-replace"))), "", self)
         replace_button.setStatusTip(self.tr("Replace X/Y/Z G-Code coordinates with value"))
-        replace_button.triggered.connect(self._aa)
+        replace_button.clicked.connect(self._handle_replace_button)
 
-        toolbar.addAction(replace_button)
+        toolbar.addWidget(replace_button)
 
         self.setStatusBar(QtWidgets.QStatusBar(self))
 
         self.gcode = None
 
-    def _aa(self, s):
-        print(s)
+    @QtCore.Slot()
+    def _handle_plus_button(self):
+        if self._specific_val_checkbox.isChecked():
+            self.gcode = [inc_coor(line, self._coor_dropdown.currentText(), self._new_coor_val.value(), only_for_val = self._specific_val_selector.value()) for line in self.gcode]
+        else:
+            self.gcode = [inc_coor(line, self._coor_dropdown.currentText(), self._new_coor_val.value()) for line in self.gcode]
+
+        self._update_gcode_viewer()
+
+    @QtCore.Slot()
+    def _handle_minus_button(self):
+        if self._specific_val_checkbox.isChecked():
+            self.gcode = [dec_coor(line, self._coor_dropdown.currentText(), self._new_coor_val.value(), only_for_val = self._specific_val_selector.value()) for line in self.gcode]
+        else:
+            self.gcode = [dec_coor(line, self._coor_dropdown.currentText(), self._new_coor_val.value()) for line in self.gcode]
+
+        self._update_gcode_viewer()
+
+    @QtCore.Slot()
+    def _handle_replace_button(self):
+        if self._specific_val_checkbox.isChecked():
+            self.gcode = [replace_coor(line, self._coor_dropdown.currentText(), self._new_coor_val.value(), only_for_val = self._specific_val_selector.value()) for line in self.gcode]
+        else:
+            self.gcode = [replace_coor(line, self._coor_dropdown.currentText(), self._new_coor_val.value()) for line in self.gcode]
+
+        self._update_gcode_viewer()
 
     def _create_io_group_box(self) -> None:
         self._io_group_box = QtWidgets.QGroupBox(self.tr("IO"))
@@ -89,24 +111,26 @@ class GCodeUtils(QtWidgets.QMainWindow):
         self._io_group_box.setLayout(layout)
 
     def _create_coor_group_box(self) -> None:
+        SELECTOR_MIN, SELECTOR_MAX = (0, 300)
+
         self._coor_group_box = QtWidgets.QGroupBox(self.tr("Coordinates"))
         layout = QtWidgets.QHBoxLayout()
 
-        coor_dropdown = QtWidgets.QComboBox()
-        coor_dropdown.addItems(['X', 'Y', 'Z'])
+        self._coor_dropdown = QtWidgets.QComboBox()
+        self._coor_dropdown.addItems(['X', 'Y', 'Z'])
 
-        new_coor_selector = QtWidgets.QSpinBox()
-        new_coor_selector.setRange(0, 300)
+        self._new_coor_val = QtWidgets.QSpinBox()
+        self._new_coor_val.setRange(SELECTOR_MIN, SELECTOR_MAX)
 
-        specific_val_checkbox = QtWidgets.QCheckBox(self.tr("Specific value only"))
+        self._specific_val_checkbox = QtWidgets.QCheckBox(self.tr("Specific value only"))
 
-        specific_val_selector = QtWidgets.QSpinBox()
-        specific_val_selector.setMinimum(0)
+        self._specific_val_selector = QtWidgets.QSpinBox()
+        self._specific_val_selector.setRange(SELECTOR_MIN, SELECTOR_MAX)
 
-        layout.addWidget(coor_dropdown)
-        layout.addWidget(new_coor_selector)
-        layout.addWidget(specific_val_checkbox)
-        layout.addWidget(specific_val_selector)
+        layout.addWidget(self._coor_dropdown)
+        layout.addWidget(self._new_coor_val)
+        layout.addWidget(self._specific_val_checkbox)
+        layout.addWidget(self._specific_val_selector)
 
         self._coor_group_box.setLayout(layout)
 
