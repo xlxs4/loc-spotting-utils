36ce88d2a65c8ac59fee318d9b01701fa6c1c1e5
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2022-11-18 19:06:36 +0200

    gui: Add formats initializer in highlighter

--- src/highlighter.py
+++ src/highlighter.py
@@ -1,9 +1,43 @@
 from PySide6.QtCore import QRegularExpression
-from PySide6.QtGui import QSyntaxHighlighter, QTextCharFormat, QFont, QColorConstants
+from PySide6.QtGui import QSyntaxHighlighter, QTextCharFormat, QFont, QColor, QColorConstants
 
 
 class Highlighter(QSyntaxHighlighter):
+    # Hack to be able to define a custom initializer.
+    # By default you can only implement the highlightBlock virtual function
+    # without messing up the way it connects to the text parent behind the scenes.
+    def __init__(self, parent=None):
+        QSyntaxHighlighter.__init__(self, parent)
+        self._initialize_formats()
+
+    def _initialize_formats(self):
+        all_formats = (
+            # name, color, bold, italic
+            ("normal", None, False, False),
+            ("keyword", QColorConstants.DarkMagenta, True, False),
+            ("builtin", QColorConstants.DarkMagenta, False, False),
+            ("comment", QColorConstants.LightGray, False, False),
+            ("coor-num", QColorConstants.DarkMagenta, False, False),
+            ("coor-val", QColorConstants.DarkMagenta, True, True)
+        )
+
+        self._formats = {}
+
+        for name, color, bold, italic in all_formats:
+            format_ = QTextCharFormat()
+            if color:
+                format_.setForeground(QColor(color))
+            if bold:
+                format_.setFontWeight(QFont.Weight.Bold)
+            if italic:
+                format_.setFontItalic(True)
+
+            self._formats[name] = format_
+
     def highlightBlock(self, text: str) -> None:
+        text_length = len(text)
+        self.setFormat(0, text_length, self._formats["normal"])
+
         builtin_format = QTextCharFormat()
         builtin_format.setFontWeight(QFont.Bold)
         builtin_format.setForeground(QColorConstants.DarkMagenta)
